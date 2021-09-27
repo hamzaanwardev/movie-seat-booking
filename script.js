49765b1f103a0c87661f88378c6a7f2049803f8f
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-28 01:48:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.seat');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+const ticketPrice = +movieSelect.value;
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat')) {
+
+    console.log(e.target);
+    }
+})
