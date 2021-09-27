8fe78ddbbfaecb09e5796545b999017dfb372833
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-28 02:08:10 +0500

    updated JS file

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value;
 
-container.addEventListener('click', (e) => {
-    if (e.target.classList.contains('seat')) {
 
-    console.log(e.target);
-    }
+// Update total And Count
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    console.log (selectedSeatsCount);
+}
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+       e.target.classList.toggle ('selected');
+
+       updateSelectedCount ();
+    } 
 })
