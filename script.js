5d3d0d351edfafc23f12fe9ca133e7169285bcc8
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-29 23:43:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
 
-    const seatsIndex = [...selectedSeats].map((seat) => {
-        return[...seats].indexOf(seat)
-    })
+    const seatsIndex = [...selectedSeats].map((seat) =>[...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
    count.innerText = selectedSeatsCount;
@@ -24,13 +25,14 @@ function updateSelectedCount() {
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
-    updateSelectedCount
+    console.log(e.target.selectedIndex);
+    updateSelectedCount ();
 
-})
+});
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
        e.target.classList.toggle ('selected');
 
        updateSelectedCount ();
-    } 
-})
+    } ;
+});
